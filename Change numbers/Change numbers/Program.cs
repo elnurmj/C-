c2dc6c502fa8ad73c45c4c3e35935859c741c4d9
unknown <elnurmj@code.edu.az>
2021-11-25 16:09:42 +0400

    Changing numbers

---
+++ Change numbers/Change numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Change_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int num1 = 5;
+            int num2 = 11;
+            num2 = num1 + num2;
+            num1 = num2 - num1;
+            num2 = num2 - num1;
+            Console.WriteLine(num1 + " " + num2);
+
+
+
+        }
+    }
+}
